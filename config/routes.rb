a6bbc9209cfe0b8ac954cafce3d1ccc6d1a968e0
Yigit Mersin <yigitmersin@gmail.com>
2022-03-21 22:37:53 +0300

    Add custom page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
+  get '/greeting', to: 'custom_pages#hello'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
